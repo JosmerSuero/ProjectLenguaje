9c351f327b248aa6d5a9e73115f94f35df895229
josmer benjamin suero sencion <josmersuero@gmail.com>
2020-05-31 21:18:34 -0400

    added commit and web project

---
+++ ProjectLenguaje.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjectLenguaje.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProjectLenguaje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectLenguaje.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
